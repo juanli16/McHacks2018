7f985d5b79dc9a14ed115e9d1a8fc4cfaa696e82
Amir Bawab <amirbawab@gmail.com>
2018-02-03 14:27:28 -0500

    Init

--- server-hooks/routes/index.js
+++ server-hooks/routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.setPath([{name: "Home"}]);
+    res.templateRender('home/home', 'Welcome page');
 });
 
 module.exports = router;
