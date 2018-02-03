558df92e8d5421de77c793989c7d3b5ae2923f4b
Amir Bawab <amirbawab@gmail.com>
2018-02-03 14:11:06 -0500

    Removed

--- server-hooks/routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/', function(req, res, next) {
-    res.setPath([{name: "Home"}]);
-    res.templateRender('home/home', 'Welcome page');
-});
-
-module.exports = router;
