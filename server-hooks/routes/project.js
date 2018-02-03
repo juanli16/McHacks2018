54fe7bbba8e0431fc24614c3917387f3a21a70e9
Amir Bawab <amirbawab@gmail.com>
2018-02-03 17:36:30 -0500

    Commit section in project view

--- server-hooks/routes/project.js
+++ server-hooks/routes/project.js
@@ -12,7 +12,7 @@ router.get('/', function(req, res, next) {
             links.push('/project/json/' + projects[i].pid);
         }
         res.addData('calheatmap_data', links),
-        res.setPath([{name: "Home", url: '/'}, {name: 'Project'}, {name: 'All'}]);
+        res.setPath([{name: "Home", url: '/'}, {name: 'Projects'}]);
         res.templateRender('project/all', 'All projects');
     });
 });
@@ -23,7 +23,7 @@ router.get('/view/:id(\\d+)', function(req, res, next) {
             res.redirect404('Project not found');
         } else {
             res.addData('project', project);
-            res.setPath([{name: "Home", url: '/'}, {name: 'Project'}, {name: 'View'}]);
+            res.setPath([{name: "Home", url: '/'}, {name: 'Projects', url: '/project'}, {name: project.name}]);
             res.templateRender('project/view', 'View project');
         }
     });
